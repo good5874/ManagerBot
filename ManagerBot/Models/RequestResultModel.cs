b0d1600030157511ec1e108813bc061bc24313db
qwertyuiopasdfghjklzxcvbnm <qweqw.qwe.88@mail.ru>
2021-06-08 20:46:35 +0300

    moved models to dal

--- ManagerBot/Models/RequestResultModel.cs
+++ ManagerBot/Models/RequestResultModel.cs
@@ -1,8 +1,10 @@
-﻿namespace ManagerBot.Models
+﻿using ManagerBot.DAL.Entity;
+
+namespace ManagerBot.Models
 {
     public class RequestResultModel
     {
         public string Message { get; set; }
-        public UserModel User { get; set; }
+        public UserEntity User { get; set; }
     }
 }
