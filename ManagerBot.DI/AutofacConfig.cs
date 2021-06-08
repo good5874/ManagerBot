f5fafb461781c34313ec1211bc9a435d7f728f3d
qwertyuiopasdfghjklzxcvbnm <qweqw.qwe.88@mail.ru>
2021-06-08 20:58:52 +0300

    add autofac

---
+++ ManagerBot.DI/AutofacConfig.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using ManagerBot.DAL.DataBase;
+
+namespace ManagerBot.DI
+{
+    public class AutofacConfig
+    {
+        public static IContainer ConfigureContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            #region Repositories
+
+            #endregion
+            #region DbContext
+            builder.RegisterType<BotDbContext>()
+                .AsSelf();
+            #endregion
+
+            return builder.Build();
+        }
+    }
+}
