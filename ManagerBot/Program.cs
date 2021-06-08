985ee2e6593697c73537f14f23f35ea318bac02d
qwertyuiopasdfghjklzxcvbnm <qweqw.qwe.88@mail.ru>
2021-06-08 17:47:52 +0300

    Добавьте файлы проекта.

---
+++ ManagerBot/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.Enums;
+
+namespace ManagerBot
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Bot bot = new Bot();
+			bot.Start();
+		}
+
+	}
+
+	public class Bot
+    {
+		private TelegramBotClient client;
+		public void Start ()
+		{
+			client = new TelegramBotClient("1854950611:AAHE-_rO_PmkM8d8RZK1kSoRgMI0_6LtroU");
+			client.OnMessage += BotOnMessageReceived;
+			client.OnMessageEdited += BotOnMessageReceived;
+			client.StartReceiving();
+			Console.ReadLine();
+			client.StopReceiving();
+		}
+
+		private async void BotOnMessageReceived(object sender, MessageEventArgs messageEventArgs)
+		{
+			var message = messageEventArgs.Message;
+			if (message?.Type == MessageType.Text)
+			{
+				await client.SendTextMessageAsync(message.Chat.Id, message.Text);
+			}
+		}
+	}
+}
