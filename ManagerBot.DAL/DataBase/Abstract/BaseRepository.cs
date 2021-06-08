90ba597a7c705d8129ea16451f0cfb32f40eed25
qwertyuiopasdfghjklzxcvbnm <qweqw.qwe.88@mail.ru>
2021-06-08 20:33:40 +0300

    add baseRepository

---
+++ ManagerBot.DAL/DataBase/Abstract/BaseRepository.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace ManagerBot.DAL.DataBase.Abstract
+{
+    public abstract class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity: class
+    {
+        private readonly BotDbContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public BaseRepository(BotDbContext  context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _dbSet = _context.Set<TEntity>();
+        }
+
+        public async Task CreateAsync(TEntity entity)
+        {
+            _dbSet.Add(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task RemoveAsync(TEntity entity)
+        {
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task RemoveByIdAsync(int id)
+        {
+            TEntity entity = await FindByIdAsync(id);
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<TEntity> FindByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task UpdateAsync(TEntity entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public IEnumerable<TEntity> GetWithInclude(params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            return Include(includeProperties).ToList();
+        }
+
+        public IEnumerable<TEntity> GetWithInclude(Func<TEntity, bool> predicate,
+            params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            var query = Include(includeProperties);
+            return query.Where(predicate).ToList();
+        }
+
+        private IQueryable<TEntity> Include(params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            IQueryable<TEntity> query = _dbSet.AsNoTracking();
+            return includeProperties
+                .Aggregate(query, (current, includeProperty) => current.Include(includeProperty));
+        }
+    }
+}
---
+++ ManagerBot.DAL/DataBase/Abstract/IBaseRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace ManagerBot.DAL.DataBase.Abstract
+{
+    public interface IBaseRepository<TEntity> where TEntity : class
+    {
+        Task CreateAsync(TEntity entity);
+        Task RemoveAsync(TEntity entity);
+        Task RemoveByIdAsync(int id);
+        Task<IEnumerable<TEntity>> GetAsync();
+        Task<TEntity> FindByIdAsync(int id);
+        Task UpdateAsync(TEntity entity);
+        IEnumerable<TEntity> GetWithInclude(params Expression<Func<TEntity, object>>[] includeProperties);
+        IEnumerable<TEntity> GetWithInclude(Func<TEntity, bool> predicate,
+            params Expression<Func<TEntity, object>>[] includeProperties);
+    }
+}
