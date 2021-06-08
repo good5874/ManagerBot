5833a4172828e37d3ea3b58b10cf5faf664e1e2b
qwertyuiopasdfghjklzxcvbnm <qweqw.qwe.88@mail.ru>
2021-06-08 20:34:14 +0300

    add db

---
+++ ManagerBot.DAL/DataBase/BotDbContext.cs
@@ -0,0 +1,25 @@
+﻿using ManagerBot.DAL.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManagerBot.DAL.DataBase
+{
+    public class BotDbContext : DbContext
+    {
+        public DbSet<OrderEntity> OrderEntities { get; set; }
+        public DbSet<AreaEntity> AreaEntities { get; set; }
+        public DbSet<TaskEntity> TaskEntities { get; set; }
+
+        public BotDbContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=tcp:trainingportal.database.windows.net,1433;Initial Catalog=ManagerBot;Persist Security Info=False;User ID=trainingportal;Password={123zxcVBNM};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        }
+    }
+}
---
+++ ManagerBot.DAL/Entities/AreaEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManagerBot.DAL.Entity
+{
+    public class AreaEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
