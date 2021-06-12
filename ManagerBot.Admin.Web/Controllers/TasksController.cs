37baeb2d7774e80afc0f8dfde0f6fdcff1c7d68c
qwertyuiopasdfghjklzxcvbnm <qweqw.qwe.88@mail.ru>
2021-06-12 13:56:41 +0300

    create web progect + add tasks controller + add user contoller + view

---
+++ ManagerBot.Admin.Web/Controllers/TasksController.cs
@@ -0,0 +1,26 @@
+﻿using ManagerBot.DAL.DataBase.Repositories.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManagerBot.Admin.Web.Controllers
+{
+    public class TasksController : Controller
+    {
+        private ITaskRepository taskRepository { get; set; }
+
+        public TasksController(ITaskRepository taskRepository)
+        {
+            this.taskRepository = taskRepository;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            var tasks = await taskRepository.GetTaskWithIncludesAsync();
+            return View(tasks.ToList());
+        }
+    }
+}
---
+++ ManagerBot.Admin.Web/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using ManagerBot.DAL.DataBase.Repositories.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManagerBot.Admin.Web.Controllers
+{
+    public class UsersController : Controller
+    {
+        private IUserRepository userRepository { get; set; }
+
+        public UsersController(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            var tasks = await userRepository.GetUsersWithRolesAsync();
+            return View(tasks.ToList());
+        }
+    }
+}
