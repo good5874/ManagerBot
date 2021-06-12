10232ecd289d0896c3592ce40b6787609b0ff825
qwertyuiopasdfghjklzxcvbnm <qweqw.qwe.88@mail.ru>
2021-06-12 12:47:59 +0300

    add TaskRepository

---
+++ ManagerBot.DAL/DataBase/Repositories/Abstract/ITaskRepository.cs
@@ -0,0 +1,11 @@
+﻿using ManagerBot.DAL.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ManagerBot.DAL.DataBase.Repositories.Abstract
+{
+    public interface ITaskRepository : IBaseRepository<TaskEntity>
+    {
+        public Task<IEnumerable<TaskEntity>> GetTaskWithIncludesAsync();
+    }
+}
---
+++ ManagerBot.DAL/DataBase/Repositories/TaskRepository.cs
@@ -0,0 +1,31 @@
+﻿using ManagerBot.DAL.DataBase.Repositories.Abstract;
+using ManagerBot.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagerBot.DAL.DataBase.Repositories
+{
+    public class TaskRepository
+        : BaseRepository<TaskEntity>,
+          ITaskRepository
+    {
+        private readonly BotDbContext context;
+
+        public TaskRepository(BotDbContext context)
+            : base(context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IEnumerable<TaskEntity>> GetTaskWithIncludesAsync()
+        {
+            return await context.Tasks
+                .Include(u => u.User)
+                .Include(o => o.Operation)
+                .ToListAsync();
+        }
+    }
+}
