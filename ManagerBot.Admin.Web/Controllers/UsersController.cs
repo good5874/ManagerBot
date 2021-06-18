a5afd347a3cfd877051f98f39c8890c8d0f9738d
qwertyuiopasdfghjklzxcvbnm <qweqw.qwe.88@mail.ru>
2021-06-18 19:27:04 +0300

    add refresh salary

--- ManagerBot.Admin.Web/Controllers/UsersController.cs
+++ ManagerBot.Admin.Web/Controllers/UsersController.cs
@@ -18,8 +18,19 @@ namespace ManagerBot.Admin.Web.Controllers
 
         public async Task<ActionResult> Index()
         {
-            var tasks = await userRepository.GetUsersWithRolesAsync();
-            return View(tasks.ToList());
+            var users = await userRepository.GetUsersWithRolesAsync();
+            return View(users.ToList());
+        }
+
+        public async Task<ActionResult> Refresh()
+        {
+            var users = await userRepository.GetUsersWithRolesAsync();
+            foreach(var user in users)
+            {
+                user.Salary = 0;
+            }
+            await userRepository.UpdateAll(users);
+            return View(nameof(Index), users.ToList());
         }
     }
 }
